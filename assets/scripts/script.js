3e6673a4addeadcd1482a867ebf267b9fc116e07
Danny Calise <dannyjcalise@gmail.com>
2019-09-25 17:39:20 -0400

    added functionality for when a user types in Philadelphia, it will spit out whether or not the 76ers are playing

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,8 @@
+// On push of the submit button, take the value of the city
+var proTeams = [];
+var phillyTeams = [];
+var phillyTeamsTonight = [];
+
 $('#submit-button').on('click', function() {
     event.preventDefault();
     var userCity = $('#city-input').val().trim();
@@ -5,11 +10,43 @@ $('#submit-button').on('click', function() {
     var queryURL1 = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + userCity;
 
     $.get(queryURL1).then(function(response) {
-        var teams = response.teams;
-        for (let i = 0; i < teams.length; i++) {
-            console.log(teams[i]);
+        phillyTeams = response.teams;
+        var leagues = ["NFL", "NBA", "NHL", "MLB"];
+        for (let i = 0; i < phillyTeams.length; i++) {
+            if (leagues.indexOf(phillyTeams[i].strLeague) !== -1) {
+                proTeams.push(phillyTeams[i].strTeam);
+            } 
         }
     });
+    const url = "https://cors-anywhere.herokuapp.com/https://api-nba-v1.p.rapidapi.com/games/date/2019-09-26";
+        const settings = {
+            url: url,
+            method: "GET",
+            headers: {
+                "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
+	            "x-rapidapi-key": "1eb7eadabemshf0789dca576eb58p1442aejsnece09022835e"
+            }
+        };
+        $.ajax(settings).then(function (response) {
+            var games = response.api.games;
+            var teams = [];
+            var hTeam;
+            var vTeam;
+            for (let j = 0; j < games.length; j++) {
+                teams.push(games[j].hTeam.fullName);
+                teams.push(games[j].vTeam.fullName);
+            }
+            for (let k = 0; k < teams.length; k++) {
+                if (proTeams.indexOf(teams[k]) !== -1) {
+                    phillyTeamsTonight.push(teams[k]);
+                } 
+            }
+        });
+        console.log(phillyTeamsTonight);
 });
+
+
+        
+        
 
 
