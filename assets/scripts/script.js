3e7f100e3e2785859bdd7250826628997a4a1f15
jcihnacik <jcihnacik@gmail.com>
2019-09-28 12:00:53 -0400

    jen branch

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,29 +1,42 @@
 // On push of the submit button, take the value of the city
-var proTeams = [];
-var phillyTeams = [];
-var phillyTeamsTonight = [];
 
+var beerGardenWeather;
 
 $('#submit-button').on('click', function () {
     event.preventDefault();
+    console.log("submitted");
+
+        var userCity = $('#city-input').val().trim();
+        console.log(userCity);
+        var openRestaurantsArray = [];
 
 
-    var userCity = $('#city-input').val().trim();
+        const queryURL = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&open_now=true&location=" + userCity;
+        console.log(queryURL);
 
-    const url = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=restaurants&location=" + userCity;
+        const settings = {
+            queryURL: queryURL,
+            method: "GET",
+            headers: {
+                "Authorization": "Bearer LdIoBm1aGT5mCUNt8oZHjlmAPaFP3OSz3RW5HEFW5IUcrqttybk1fSx8NQgRIwvg7G8JRyVR9-yRda_5MKXxtGFu9p1QhCMeQxCdxRZt2SqM8CiFJcIdPdUgrcGKXXYx"
+            }
+        };
+        $.ajax(settings).then(function (response) {
+            console.log("hello");
+            console.log(response.businesses[1].alias)
+            var openRestaurants = response.businesses;
+            for (let i = 0; i < openRestaurants.length; i++) {
+                openRestaurantsArray.push(openRestaurants.name);
+                console.log(openRestaurantsArray);
+            } if (beerGardenWeather = true) {
+                console.log(response.businesses)
 
-    const settings = {
-        url: url,
-        method: "GET",
-        headers: {
-            "Authorization": "Bearer LdIoBm1aGT5mCUNt8oZHjlmAPaFP3OSz3RW5HEFW5IUcrqttybk1fSx8NQgRIwvg7G8JRyVR9-yRda_5MKXxtGFu9p1QhCMeQxCdxRZt2SqM8CiFJcIdPdUgrcGKXXYx"
-        }
-    };
-    $.ajax(settings).then(function (response) {
-        console.log("hello");
-        console.log(response.businesses[1].alias)
-    })
+            }
+        });
 })
+
+
+
 
 
 // var restaurantsDiv = $("<div>").addClass("card");
@@ -61,154 +74,6 @@ $('#submit-button').on('click', function () {
 
 // // Append the table row to the table body
 // $("#results-area").append(eventDiv);
-
-
-
-
-
-
-// var queryURL1 = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + userCity;
-
-// $.get(queryURL1).then(function (response) {
-//     phillyTeams = response.teams;
-//     var leagues = ["NFL", "NBA", "NHL", "MLB"];
-//     for (let i = 0; i < phillyTeams.length; i++) {
-//         if (leagues.indexOf(phillyTeams[i].strLeague) !== -1) {
-//             proTeams.push(phillyTeams[i].strTeam);
-//         }
-//     }
-// });
-// var date = $('#date-input').val().trim();
-// const url = "https://cors-anywhere.herokuapp.com/https://api-nba-v1.p.rapidapi.com/games/date/" + date;
-// const settings = {
-//     url: url,
-//     method: "GET",
-//     headers: {
-//         "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-//         "x-rapidapi-key": "1eb7eadabemshf0789dca576eb58p1442aejsnece09022835e"
-//     }
-// };
-// $.ajax(settings).then(function (response) {
-//     var games = response.api.games;
-//     var teams = [];
-//     var hTeam;
-//     var vTeam;
-//     for (let j = 0; j < games.length; j++) {
-//         teams.push(games[j].hTeam.fullName);
-//         teams.push(games[j].vTeam.fullName);
-//     }
-//     for (let k = 0; k < teams.length; k++) {
-//         if (proTeams.indexOf(teams[k]) !== -1) {
-//             phillyTeamsTonight.push(teams[k]);
-//         }
-//     }
-//     console.log(phillyTeamsTonight);
-// });
-
-///////
-var gameTonight = false;
-var cityTeamsTonight = [];
-var cityTeams = [];
-var sportsTeams = [];
-var getAvailableTeams = function (city) {
-    var queryURLTeams = 'https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=' + city;
-    $.get(queryURLTeams).then(function (response) {
-        cityTeams = response.teams;
-        var leagues = ["NFL", "NBA", "NHL", "MLB"];
-        for (let i = 0; i < cityTeams.length; i++) {
-            if (leagues.indexOf(cityTeams[i].strLeague) !== -1) {
-                proTeams.push(cityTeams[i].strTeam);
-            }
-        }
-    });
-}
-
-var getEventsBySport = function (sportsID, date) {
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://therundown-therundown-v1.p.rapidapi.com/sports/" + sportsID + "/events/" + date + "?include=all_periods&include=scores",
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "therundown-therundown-v1.p.rapidapi.com",
-            "x-rapidapi-key": "1eb7eadabemshf0789dca576eb58p1442aejsnece09022835e"
-        }
-    }
-    $.ajax(settings).done(function (response) {
-        var events = response.events;
-        console.log(events);
-        for (let i = 0; i < events.length; i++) {
-            var teams = events[i].teams;
-            for (let j = 0; j < teams.length; j++) {
-                sportsTeams.push(teams[j].name);
-            }
-        }
-        // console.log(sportsTeams);
-        for (let j = 0; j < sportsTeams.length; j++) {
-            // if (proTeams.indexOf(sportsTeams[j]) !== -1) {
-            //     if (cityTeamsTonight.indexOf(baseballTeams[j] === -1)) {
-            //     cityTeamsTonight.push(sportsTeams[j]);
-            //     }
-            // };
-            
-            if (proTeams.includes(sportsTeams[j])) {
-                gameTonight = true;
-                console.log("Your team is playing tonight");
-                cityTeamsTonight.push(sportsTeams[j]);
-            } else {
-                // console.log("Your team is NOT playing tonight");
-            }
-        }
-        console.log(sportsTeams);
-    });
-    
-}
-$('#submit-button').on('click', function () {
-    event.preventDefault();
-    var userCity = $('#city-input').val().trim();
-    var date = $('#date-input').val().trim();
-    getAvailableTeams(userCity);
-    getEventsBySport(3, date);
-    console.log(proTeams);
-    console.log(sportsTeams);
-});
-
-
-     $.get(queryURLBasketball).then(function(response) {
-        cityTeams = response.teams;
-        var leagues = ["NFL", "NBA", "NHL", "MLB"];
-        for (let i = 0; i < cityTeams.length; i++) {
-            if (leagues.indexOf(cityTeams[i].strLeague) !== -1) {
-                 proTeams.push(cityTeams[i].strTeam);
-             } 
-         }
-     });
-     var date = $('#date-input').val().trim();
-     const url = "https://cors-anywhere.herokuapp.com/https://api-nba-v1.p.rapidapi.com/games/date/" + date;
-        const settings = {
-    //         url: url,
-    //         method: "GET",
-    //         headers: {
-    //             "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-	//             "x-rapidapi-key": "1eb7eadabemshf0789dca576eb58p1442aejsnece09022835e"
-    //         }
-    //     };
-    //     $.ajax(settings).then(function (response) {
-    //         var games = response.api.games;
-    //         var teams = [];
-    //         var hTeam;
-    //         var vTeam;
-    //         for (let j = 0; j < games.length; j++) {
-    //             teams.push(games[j].hTeam.fullName);
-    //             teams.push(games[j].vTeam.fullName);
-    //         }
-    //         for (let k = 0; k < teams.length; k++) {
-    //             if (proTeams.indexOf(teams[k]) !== -1) {
-    //                 cityTeamsTonight.push(teams[k]);
-    //             } 
-    //         }
-    //         console.log(cityTeamsTonight);
-    //     });
 
 
 
