d0632ca45246ad4ad6e6aeed6797f85bf94d955e
Danny Calise <dannyjcalise@gmail.com>
2019-09-28 10:35:07 -0400

    layout for eat screen

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
 // On push of the submit button, take the value of the city
 var proTeams = [];
-// var gameTeams = [];
+var gameTonight = false;
 var cityTeamsTonight = [];
 var cityTeams = [];
 var sportsTeams = [];
@@ -15,7 +15,6 @@ var getAvailableTeams = function (city) {
             }
         }
     });
-
 }
 
 var getEventsBySport = function (sportsID, date) {
@@ -31,21 +30,32 @@ var getEventsBySport = function (sportsID, date) {
     }
     $.ajax(settings).done(function (response) {
         var events = response.events;
+        console.log(events);
         for (let i = 0; i < events.length; i++) {
             var teams = events[i].teams;
             for (let j = 0; j < teams.length; j++) {
                 sportsTeams.push(teams[j].name);
             }
         }
+        // console.log(sportsTeams);
         for (let j = 0; j < sportsTeams.length; j++) {
-            if (proTeams.indexOf(sportsTeams[j]) !== -1) {
-                // if (cityTeamsTonight.indexOf(baseballTeams[j] === -1)) {
+            // if (proTeams.indexOf(sportsTeams[j]) !== -1) {
+            //     if (cityTeamsTonight.indexOf(baseballTeams[j] === -1)) {
+            //     cityTeamsTonight.push(sportsTeams[j]);
+            //     }
+            // };
+            
+            if (proTeams.includes(sportsTeams[j])) {
+                gameTonight = true;
+                console.log("Your team is playing tonight");
                 cityTeamsTonight.push(sportsTeams[j]);
-                // }
-            };
+            } else {
+                // console.log("Your team is NOT playing tonight");
+            }
         }
-        console.log(cityTeamsTonight);
+        console.log(sportsTeams);
     });
+    
 }
 $('#submit-button').on('click', function () {
     event.preventDefault();
@@ -53,47 +63,6 @@ $('#submit-button').on('click', function () {
     var date = $('#date-input').val().trim();
     getAvailableTeams(userCity);
     getEventsBySport(3, date);
+    console.log(proTeams);
+    console.log(sportsTeams);
 });
-
-    // $.get(queryURLBasketball).then(function(response) {
-    //     cityTeams = response.teams;
-    //     var leagues = ["NFL", "NBA", "NHL", "MLB"];
-    //     for (let i = 0; i < cityTeams.length; i++) {
-    //         if (leagues.indexOf(cityTeams[i].strLeague) !== -1) {
-    //             proTeams.push(cityTeams[i].strTeam);
-    //         } 
-    //     }
-    // });
-    // var date = $('#date-input').val().trim();
-    // const url = "https://cors-anywhere.herokuapp.com/https://api-nba-v1.p.rapidapi.com/games/date/" + date;
-    //     const settings = {
-    //         url: url,
-    //         method: "GET",
-    //         headers: {
-    //             "x-rapidapi-host": "api-nba-v1.p.rapidapi.com",
-	//             "x-rapidapi-key": "1eb7eadabemshf0789dca576eb58p1442aejsnece09022835e"
-    //         }
-    //     };
-    //     $.ajax(settings).then(function (response) {
-    //         var games = response.api.games;
-    //         var teams = [];
-    //         var hTeam;
-    //         var vTeam;
-    //         for (let j = 0; j < games.length; j++) {
-    //             teams.push(games[j].hTeam.fullName);
-    //             teams.push(games[j].vTeam.fullName);
-    //         }
-    //         for (let k = 0; k < teams.length; k++) {
-    //             if (proTeams.indexOf(teams[k]) !== -1) {
-    //                 cityTeamsTonight.push(teams[k]);
-    //             } 
-    //         }
-    //         console.log(cityTeamsTonight);
-    //     });
-
-
-
-
-
-
-
